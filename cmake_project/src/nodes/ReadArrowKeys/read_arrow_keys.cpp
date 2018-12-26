14ddfd84fce0850b6d967674557bcd95329ff981
Sergio Garcia <sergio.garcia@gtri.gatech.edu>
2018-12-25 20:31:27 -0500

    complete | all nodes building with cmake

--- cmake_project/src/nodes/ReadArrowKeys/read_arrow_keys.cpp
+++ cmake_project/src/nodes/ReadArrowKeys/read_arrow_keys.cpp
@@ -4,43 +4,43 @@
 
 using namespace std;
 
-int main (int argc, char *argv[])
-{
-  printf("this is a test... \n");
-  
-  // initialization
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
-  
-  // declare variables
-  int c = 0;
-  printw("Enter a key... %d\n", c);
-  while(1) {
-    
-    // read input
-    c = getch();
+int main (int argc, char *argv[]) {
 
-    // do something depending on input key
-    switch(c) {
-    case KEY_UP:
-      printw("Up\n");
-      break;
-    case KEY_DOWN:
-      printw("Down\n");
-      break;
-    case KEY_LEFT:
-      printw("Left\n");
-      break;
-    case KEY_RIGHT:
-      printw("Right\n");
-      break;
-    default:
-      printw("not an arrow\n");
-      break;
+    printf("this is a test... \n");
+
+    // initialization
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    // declare variables
+    int c = 0;
+    printw("Enter a key... %d\n", c);
+    while(1) {
+
+        // read input
+        c = getch();
+
+        // do something depending on input key
+        switch(c) {
+        case KEY_UP:
+            printw("Up\n");
+            break;
+        case KEY_DOWN:
+            printw("Down\n");
+            break;
+        case KEY_LEFT:
+            printw("Left\n");
+            break;
+        case KEY_RIGHT:
+            printw("Right\n");
+            break;
+        default:
+            printw("not an arrow\n");
+            break;
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
