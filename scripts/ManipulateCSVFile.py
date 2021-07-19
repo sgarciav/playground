e455212f537d34f5af073703c8bc8d39ed455b8f
Sergio Garcia-Vergara <sergiodotgarcia@gmail.com>
2021-07-19 09:37:40 -0400

    fully working script to manipulate csv files

--- scripts/ManipulateCSVFile.py
+++ scripts/ManipulateCSVFile.py
@@ -2,20 +2,39 @@
 
 # system includes
 import csv
+import pandas
 
 class ManipulateCSVFile():
     def __init__(self, filename, verbose = False):
         self.filename = filename
         self.verbose = verbose
 
+
     def print_contents(self):
         ''' print the contents of the csv file '''
-        if (self.verbose):
-            print("-- Contents of the csv file:")
+        if self.verbose:
+            print("-- Contents of the csv file: {}".format(self.filename))
         # open the CSV file
         with open(self.filename, mode = 'r') as file:
             # reading the CSV file
             csv_file = csv.reader(file)
-
             for lines in csv_file:
                 print(lines)
+
+
+    def get_column_data(self, column_name):
+        ''' return the contents in the column as a numpy array - return False if it doesn't exist '''
+        # check if the column exists in the current file
+        all_data = pandas.read_csv(self.filename)
+        if column_name not in all_data.columns:
+            print("-- ERROR! Column {} not found in {}".format(column_name, self.filename))
+            return False
+
+        # get the column data
+        df = pandas.read_csv(self.filename, usecols=[column_name])
+        if self.verbose:
+            print("-- Column {} from {} obtained successfuly".format(column_name, self.filename))
+
+        # convert panda data frame to numpy array
+        column_data_array = df.to_numpy()
+        return column_data_array
--- scripts/test_csv_argparser.py
+++ scripts/test_csv_argparser.py
@@ -12,12 +12,22 @@ def main():
     parser.add_argument('-f', '--filename',
                         type=str,
                         default=None,
-                        help='Relative path to the CSV to manipulate.')
+                        help='Absolute or relative path to the CSV to manipulate.')
+    parser.add_argument('-c', '--column-name',
+                        type=str,
+                        default="a",
+                        help='Name of the column of interest.')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        default=False,
+                        help='Print debug messages.')
     args = parser.parse_args()
 
     # create the CSV object
-    csv_obj = ManipulateCSVFile.ManipulateCSVFile(args.filename, True)
+    csv_obj = ManipulateCSVFile.ManipulateCSVFile(args.filename, args.verbose)
     csv_obj.print_contents()
+    column_data = csv_obj.get_column_data(args.column_name)
+    print("-- Column data: {}".format(column_data))
 
 
 if __name__ == '__main__':
