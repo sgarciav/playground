f6bc01dcccdc1dd6fa5230da08719e1f47b64dc3
Sergio Garcia <sergiodotgarcia@gmail.com>
2024-08-29 16:25:55 -0400

    minor updates to csv argparser demo

--- scripts/test_csv_argparser.py
+++ scripts/test_csv_argparser.py
@@ -4,6 +4,9 @@
 import sys
 import argparse
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 # local includes
 import ManipulateCSVFile
 
@@ -25,9 +28,24 @@ def main():
 
     # create the CSV object
     csv_obj = ManipulateCSVFile.ManipulateCSVFile(args.filename, args.verbose)
-    csv_obj.print_contents()
+    if args.verbose:
+        csv_obj.print_contents()
     column_data = csv_obj.get_column_data(args.column_name)
-    print("-- Column data: {}".format(column_data))
+    if args.verbose:
+        print("-- Column data: {}".format(column_data))
+
+    # plot
+    x = []
+    for i in range(0,len(column_data)):
+        x.append(i)
+    x = np.array(x)
+
+    plt.plot(x, column_data)
+    # plt.plot(x2, y2, 'red')
+    plt.grid()
+    plt.show()
+
+
 
 
 if __name__ == '__main__':
