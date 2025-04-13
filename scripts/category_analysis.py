ec94aedf7aa2d3213c2c6d706311019f33639184
Sergio Garcia <sergiodotgarcia@gmail.com>
2025-04-13 15:07:44 -0400

    Fully working version of category analysis script

--- scripts/category_analysis.py
+++ scripts/category_analysis.py
@@ -11,12 +11,13 @@ import numpy as np
 import ManipulateCSVFile
 
 categories = {
-    'Food' : 0,
-    'Shopping' : 0,
+    'Flight' : 0,
     'Auto' : 0,
     'Hotel' : 0,
-    'Air' : 0,
+    'Shopping' : 0,
+    'Entertainment' : 0,
     'Misc' : 0,
+    'Food' : 0,
 }
 
 def main():
@@ -36,17 +37,27 @@ def main():
     # Order of data: Date, Name, Price
     grouped_list = list(zip(*[iter(column_data.tolist())]*3))
 
+    total_spending = 0
     for kv in grouped_list:
         name = str(kv[1])
         price = float(kv[2][0])
+        total_spending += price
+
+        counted = False
         for cat in categories:
             if cat in name:
                 categories[cat] += price
+                counted = True
                 break
 
-    # Print results
-    print(categories)
+        if not counted:
+            print(f'Item ({name}) not counted')
 
+    # Print results
+    print('')
+    print(f'Total travel spend: ${total_spending}')
+    for c in categories.items():
+        print(c)
 
 if __name__ == '__main__':
     sys.exit(main())
